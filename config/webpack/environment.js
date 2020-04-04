948a10b62b3516d332771d3cede6c8313a04f95a
dacardenasa <diego.cardenasaleg@gmail.com>
2020-04-04 08:49:47 -0500

    Update webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,11 +4,22 @@ const webpack         = require('webpack')
 
 environment.loaders.append('erb', erb)
 
+/*
 environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery'
+  })
+)
+*/
+
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
   })
 )
 
